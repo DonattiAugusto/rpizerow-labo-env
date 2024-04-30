b7cb9b0b5f762475b04af520d46bc49dfd34f5bf
DonattiAugusto <83584451+DonattiAugusto@users.noreply.github.com>
2024-04-30 10:56:52 -0300

    "docs: comenté el código"

--- actividades_python/ej_01/main.py
+++ actividades_python/ej_01/main.py
@@ -1,11 +1,10 @@
 from gpiozero import LED, Button
 from signal import pause
-#importo las librerias
+#importación de las librerias para usar
 led = LED(26)
 button = Button(18)
-#declaro los pines del led y el botón, y les asigno una variable
+#establezco que pines voy a usar del botón y del led, y les pongo el igual para que sean una variable
 button.when_pressed = led.on
 button.when_released = led.off
-#Declaro que el led se encienda o apague cuando el boton esta
-#presionado o suelto respectivamente
+#establezco que cuando el botón esté apretado o suelto, el led esté prendido o apagado
 pause()
